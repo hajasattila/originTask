8b2e43baf56846b1856599e245471fc829daeafa
Hajas Attila <hajaattila@gmail.com>
2023-09-26 01:56:54 +0200

    categories wip

--- Assets/js/cards.js
+++ Assets/js/cards.js
@@ -1,18 +1,5 @@
 // Hónapok tömbje
-const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Okt",
-  "Nov",
-  "Dec",
-];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"];
 
 // Az API hívása és adatok megjelenítése
 fetch("https://api.slingacademy.com/v1/sample-data/products")
@@ -29,30 +16,67 @@ fetch("https://api.slingacademy.com/v1/sample-data/products")
     slicedProducts.forEach((product) => {
       // Dátum formázása
       const date = new Date(product.created_at);
-      const formattedDate = `${date.getDate().toString().padStart(2, "0")}.${
-        months[date.getMonth()]
-      }.${date.getFullYear()}`;
+      const formattedDate = `${date.getDate().toString().padStart(2, "0")}.${months[date.getMonth()]}.${date.getFullYear()}`;
 
       const productElement = document.createElement("div");
-      productElement.classList.add("p-4", "bg-white", "rounded"); // Keret eltávolítva
+      productElement.classList.add(
+        "bg-white",
+        "rounded",
+        "max-w-screen-2xl" // Hozzáadva a szélességi osztály
+      );
       productElement.innerHTML = `
-                 <div class="bg-white rounded">
-                     <img src="${product.photo_url}" alt="${product.name}" class="w-full h-auto">
-                     <p class="text-gray-500 mt-2">${formattedDate}</p>
-                     <p class="text-xl font-semibold mt-2">${product.name}</p>
-                 </div>
-             `;
-      /* <p class="text-blue-500 mt-2">${product.category}</p> */
+        <div class="bg-white rounded">
+          <img src="${product.photo_url}" alt="${product.name}" class="w-full h-auto">
+          <p class="text-gray-500 mt-2">${formattedDate}</p>
+          <p class="text-2xl font-semibold mt-2">${product.name}</p>
+        </div>
+      `;
       container.appendChild(productElement);
+
+      // Kategória hozzáadása a tömbhöz
+      categories.push(product.category);
     });
 
+    // Kategóriák egyedülállóvá tétele a Set használatával
+    const uniqueCategories = Array.from(new Set(categories));
+
+    // Kategóriák megjelenítése
+    displayCategories(uniqueCategories);
+
     // Frissítjük az üzenetet az API elérésének sikerességével
-    document.getElementById("apiMessage").textContent =
-      "Az API elérve és az adatok megjelenítve!";
+    document.getElementById("apiMessage").textContent = "Az API elérve és az adatok megjelenítve!";
   })
   .catch((error) => {
     console.error("Hiba történt:", error);
     // Frissítjük az üzenetet, ha hiba történt
-    document.getElementById("apiMessage").textContent =
-      "Hiba történt az API elérése során.";
+    document.getElementById("apiMessage").textContent = "Hiba történt az API elérése során.";
   });
+
+// Kategóriák tömb
+const categories = [];
+
+// Kategóriák megjelenítése
+function displayCategories(categories) {
+  const mobileCategoryList = document.getElementById("mobileCategoryList");
+  const lgCategoryList = document.querySelector(".lg.flex.space-x-4");
+
+  categories.forEach((category) => {
+    // Kategória hozzáadása a mobil nézethez
+    const mobileListItem = document.createElement("li");
+    mobileListItem.textContent = category;
+    mobileListItem.classList.add("px-4", "py-2");
+    mobileCategoryList.appendChild(mobileListItem);
+
+    // Kategória hozzáadása a nagy képernyőhöz
+    const lgListItem = document.createElement("li");
+    lgListItem.textContent = category;
+    lgListItem.classList.add("mb-1");
+    lgCategoryList.appendChild(lgListItem);
+  });
+}
+
+/* Categories */
+function toggleMobileCategories() {
+  const mobileCategoryList = document.getElementById("mobileCategoryList");
+  mobileCategoryList.classList.toggle("hidden");
+}
--- Assets/js/carosuel.js
+++ Assets/js/carosuel.js
@@ -8,9 +8,7 @@ let photos = [];
 // Függvény az API lekérdezéséhez
 const fetchPhotos = async () => {
   try {
-    const response = await fetch(
-      "https://api.slingacademy.com/v1/sample-data/photos"
-    );
+    const response = await fetch("https://api.slingacademy.com/v1/sample-data/photos");
     const data = await response.json();
 
     if (data.success && data.photos.length > 0) {
@@ -27,62 +25,61 @@ const fetchPhotos = async () => {
 
 // Függvény a karusell frissítéséhez
 const updateCarousel = (photos) => {
-    // Töröld a korábbi dia elemeket
-    carousel.innerHTML = "";
-  
-    // Megkapjuk a szülő div szélességét és magasságát
-    const parentWidth = carousel.clientWidth;
-    const parentHeight = carousel.clientHeight;
-  
-    // Iterálj az API által visszaadott fotókon
-    photos.forEach((photo, index) => {
-      const inputId = `carousel-${index + 1}`;
-  
-      // Hozz létre input elemet és dia elemet
-      const input = document.createElement("input");
-      input.setAttribute("class", "carousel-open");
-      input.setAttribute("type", "radio");
-      input.setAttribute("id", inputId);
-      input.setAttribute("name", "carousel");
-      input.setAttribute("aria-hidden", "true");
-      input.setAttribute("hidden", "");
-  
-      const div = document.createElement("div");
-      div.setAttribute("class", "carousel-item");
-  
-      // Hozz létre egy img elemet a fotóhoz
-      const img = document.createElement("img");
-      img.setAttribute("src", photo.url);
-      img.setAttribute("alt", photo.title);
-  
-      // Állítsd be a kép szélességét a szülő div szélességére
-      img.style.width = `${parentWidth}px`;
-  
-      // Állítsd be a kép magasságát a szülő div magasságának felére
-      img.style.height = `40rem`;
-  
-      // Hozz létre egy felirat div-et a fotó címével
-      const caption = document.createElement("div");
-      caption.setAttribute("class", "carousel-caption");
-      caption.textContent = photo.title;
-  
-      div.appendChild(img);
-  
-      // Adj hozzá a dia elemeket a karusellhez
-      carousel.appendChild(input);
-      carousel.appendChild(div);
-  
-      // Kövessük, hogy hányadik képnél jár a slider
-      if (index === currentIndex) {
-        input.setAttribute("checked", "checked");
-      }
-    });
-  
-    // Frissítsd a képaláírást és a képszámot
-    updatePhotoDescription(photos[currentIndex]);
-    updatePhotoCount(currentIndex + 1, photos.length);
-  };
-  
+  // Töröld a korábbi dia elemeket
+  carousel.innerHTML = "";
+
+  // Megkapjuk a szülő div szélességét és magasságát
+  const parentWidth = carousel.clientWidth;
+  const parentHeight = carousel.clientHeight;
+
+  // Iterálj az API által visszaadott fotókon
+  photos.forEach((photo, index) => {
+    const inputId = `carousel-${index + 1}`;
+
+    // Hozz létre input elemet és dia elemet
+    const input = document.createElement("input");
+    input.setAttribute("class", "carousel-open");
+    input.setAttribute("type", "radio");
+    input.setAttribute("id", inputId);
+    input.setAttribute("name", "carousel");
+    input.setAttribute("aria-hidden", "true");
+    input.setAttribute("hidden", "");
+
+    const div = document.createElement("div");
+    div.setAttribute("class", "carousel-item");
+
+    // Hozz létre egy img elemet a fotóhoz
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.url);
+    img.setAttribute("alt", photo.title);
+
+    // Állítsd be a kép szélességét a szülő div szélességére
+    img.style.width = `${parentWidth}px`;
+
+    // Állítsd be a kép magasságát a szülő div magasságának felére
+    img.style.height = `40rem`;
+
+    // Hozz létre egy felirat div-et a fotó címével
+    const caption = document.createElement("div");
+    caption.setAttribute("class", "carousel-caption");
+    caption.textContent = photo.title;
+
+    div.appendChild(img);
+
+    // Adj hozzá a dia elemeket a karusellhez
+    carousel.appendChild(input);
+    carousel.appendChild(div);
+
+    // Kövessük, hogy hányadik képnél jár a slider
+    if (index === currentIndex) {
+      input.setAttribute("checked", "checked");
+    }
+  });
+
+  // Frissítsd a képaláírást és a képszámot
+  updatePhotoDescription(photos[currentIndex]);
+  updatePhotoCount(currentIndex + 1, photos.length);
+};
 
 // Függvény a képaláírás frissítéséhez
 const updatePhotoDescription = (photo) => {
